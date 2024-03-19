bc451c908a0829adde7fb1c672e21b30b6fd9096
Alejandro Lucas <alejandrolucas161@gmail.com>
2024-03-19 16:04:07 -0300

    Routes created and icons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { FunctionComponent } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: FunctionComponent = () => {
-  return <div>Challenge</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>Home</>} />
+        <Route path={`/detail-stockSymbol`} element={<></>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
